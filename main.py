4e0314aff2eb4f04cebcf0192433e2bfb699fe55
Salvador Guzman <guzmansalv@gmail.com>
2021-03-13 13:07:20 -0800

    get port

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+import atexit
+import concurrent
+import concurrent.futures as futures
+import grpc
 import logging
+import os
 
 import server_pb2
 import server_pb2_grpc
+
+
+port: str = None
+
+
+def init_env() -> None:
+    global port
+    port = os.environ['CONF_PORT']
+
+    logging.info('Found CONF_PORT at %s', port)
+
+
+def init_atexit() -> None:
+    def end():
+        logging.info('bye')
+
+    atexit.register(end)
 
 
 def init_logging() -> None:
@@ -10,11 +32,13 @@ def init_logging() -> None:
         level=logging.INFO,
         datefmt='%Y-%m-%d %H:%M:%S')
 
-    logging.info('hello')
+    logging.info('hi')
 
 
 def init() -> None:
     init_logging()
+    init_atexit()
+    init_env()
 
 
 def main() -> None:
